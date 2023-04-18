58d41cee3f525749dcf18309d2d9e0e784fb6df5
Radostina Anastasova <ina_rd@abv.bg>
2023-04-18 22:46:18 +0300

    first commit

---
+++ Pages/SumNumbersPage.cs
@@ -0,0 +1,62 @@
+﻿using OpenQA.Selenium;
+using System.Security.Principal;
+
+namespace WebDriverCalculatorPOM.Pages
+{
+    public class SumNumbersPage
+    {
+        private WebDriver driver;
+        //private const string baseUrl = "https://sum-numbers.softuniqa.repl.co/";
+        private const string baseUrl = "http://softuni-qa-loadbalancer-2137572849.eu-north-1.elb.amazonaws.com/number-calculator/";
+        
+        public SumNumbersPage(WebDriver driver) 
+        {
+            this.driver = driver;
+        }
+
+        public IWebElement Field1 => driver.FindElement(By.Id("number1"));
+        //public IWebElement Field1 { get { return driver.FindElement(By.Id("number1")); } }
+        public IWebElement OperationField => driver.FindElement(By.Id("operation"));
+        public IWebElement Field2 => driver.FindElement(By.Id("number2"));
+        public IWebElement CalcButton => driver.FindElement(By.Id("calcButton"));
+        public IWebElement ResetButton => driver.FindElement(By.Id("resetButton"));
+        public IWebElement ResultLabel => driver.FindElement(By.Id("result"));
+
+        public void Open() 
+        {
+            driver.Navigate().GoToUrl(baseUrl);
+        }
+
+        public string GetPageTitle() 
+        {
+            return driver.Title;
+        }
+
+        public bool IsPageOpen() 
+        {
+            return driver.Url == baseUrl;
+        }
+
+        public string CalculateNumbers(string firstValue, string operation, string secondValue) 
+        {
+            Field1.SendKeys(firstValue);
+            OperationField.SendKeys(operation);
+            Field2.SendKeys(secondValue);
+
+            CalcButton.Click();
+
+            return ResultLabel.Text;
+        }
+
+        public bool IsFormEmpty() 
+        {
+            bool empty = (Field1.Text == "" && Field2.Text == "");
+            return empty;
+        }
+
+        public void ResetForm() 
+        {
+            ResetButton.Click();
+        }
+    }
+}
---
+++ Tests/BaseTest.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WebDriverCalculatorPOM.Tests
+{
+    public class BaseTest
+    {
+        protected WebDriver driver;
+
+        [SetUp]
+        public void Setup() 
+        {
+            this.driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Quit();
+        }
+    }
+}
